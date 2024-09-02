4d86e627a375a5d900ebb06493421987cb939026
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-09-02 15:28:44 +0900

    save
    save

--- src/main/java/com/example/likecomment/Controller/CommentController.java
+++ src/main/java/com/example/likecomment/Controller/CommentController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/comment")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:8080"}, allowCredentials = "true")
+//@CrossOrigin(origins = {"http://localhost:8080"}, allowCredentials = "true")
 public class CommentController {
 
     @Autowired
